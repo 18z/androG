a39faf7d35512bf7b38fd1b6f9eebef4caf2bee3
KunYuChen <18z@users.noreply.github.com>
2019-10-04 14:09:41 +0800

    Add files via upload

---
+++ visualize_decompier.sh
@@ -0,0 +1,2 @@
+pydeps decompiler.py --reverse -xx androguard.core androguard.decompiler.dad -x pygments networkx asn1crypto lxml pydot --show-deps --max-bacon=6 -o reverse.svg
+pydeps decompiler.py -xx androguard.core androguard.decompiler.dad -x pygments networkx asn1crypto lxml pydot --show-deps --max-bacon=6 -o default.svg
