69d2b9104eb336b3e94e0fc2e8965983bf6a2b5b
KunYuChen <xspiritualx@gmail.com>
2019-10-07 14:09:49 +0800

    add svgs and script

---
+++ visualizePY.sh
@@ -0,0 +1,13 @@
+# keyfile = "androconf.py"
+
+pydeps $1 \
+    --reverse -xx androguard.core androguard.decompiler.dad \
+    -x pygments networkx asn1crypto lxml pydot \
+    --show-deps --max-bacon=6 \
+    -o ${1}_reverse.svg
+
+pydeps $1 \
+    -xx androguard.core androguard.decompiler.dad \
+    -x pygments networkx asn1crypto lxml pydot \
+    --show-deps --max-bacon=6 \
+    -o ${1}_default.svg
