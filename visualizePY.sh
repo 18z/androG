4126fb30b55ee37c7bbdc05a91c3065b29e833a1
KunYuChen <xspiritualx@gmail.com>
2019-10-07 14:29:55 +0800

    add bacon

--- visualizePY.sh
+++ visualizePY.sh
@@ -3,11 +3,11 @@
 pydeps $1 \
     --reverse -xx androguard.core androguard.decompiler.dad \
     -x pygments networkx asn1crypto lxml pydot \
-    --show-deps --max-bacon=6 \
+    --show-deps --max-bacon=$2 \
     -o ${1}_reverse.svg
 
 pydeps $1 \
     -xx androguard.core androguard.decompiler.dad \
     -x pygments networkx asn1crypto lxml pydot \
-    --show-deps --max-bacon=6 \
+    --show-deps --max-bacon=$2 \
     -o ${1}_default.svg
